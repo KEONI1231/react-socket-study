ad8270c47781c149a4f0672afa0221def48dc1b8
keoni <kimkeonhwi991231@gmail.com>
2023-07-23 16:33:11 +0900

    first commit and complete

---
+++ server/index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const PORT = process.env.PORT || 5001;
+const router = require("./router");
+
+const app = express();
+app.use(cors());
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("we have a new connection!!!");
+
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+    if (error) return callback(error);
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to the room ${user.room}`,
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name}, has joined!` });
+    socket.join(user.room);
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+
+    console.log(name, room);
+    callback();
+  });
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit("message", { user: user.name, text: message });
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+    callback();
+  });
+  socket.on("disconnect", () => {
+    console.log("User had left!!");
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "admin",
+        text: `${user.name} has left`,
+      });
+    }
+  });
+});
+
+app.use(router);
+server.listen(PORT, () => console.log(`server jas started on port ${PORT}`));
